c7915f306854cd6543497398b9dc79429a5d8bad
Joaquim-SNeto <netojoaquimjj@gmail.com>
2025-11-06 18:15:41 -0300

    up

--- telemetriaTelecomando/gamaSat/CalopSat/main.cpp
+++ telemetriaTelecomando/gamaSat/CalopSat/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
 
     std::cout << "\nTrying to initialize the LoRa module\n";
 
-    //initRFModule();
+    initRFModule();
 
     std::cout << "\nDevice initiated successfully\n";
 
